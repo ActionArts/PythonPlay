a63504af24032999c274a0582c7ef206d0be8739
Diane O'Connell <diane.oconnell@gmail.com>
2015-04-16 21:02:46 -0400

    bug fixes to CSV Parser, clean-up

--- CsvParser.py
+++ CsvParser.py
@@ -14,36 +14,37 @@
 # Morrissey                  60      Musician  
 
 
-# Read file lines into list
 f = open('test_csv.txt','r')
 lines = f.readlines()
 f.close()
 
 parsedCsv = ""
-spacesPadding = " " * 4
-longestValueInLine = []
+pad = " "
+longestValueInLine = 1
 
 #loop through each line, split on the comma, and store (the length of the longest string) for each column
 for line in lines:
 	values = line.split(',')
-	longestValue = 0
 	for value in values:
-		if value != "" and len(value) > longestValue:
-			longestValue = len(value)
-	longestValueInLine.append(longestValue)
-
+		if value != "" and len(value) > longestValueInLine:
+			longestValueInLine = len(value)
 
 #loop through each line again, split on the comma, and print string + spaces for (longest string - string.len()
-for index, line in enumerate(lines):
-	toPrint = ""
+for index, ln in enumerate(lines):
+	formattedLine = ""
 	delim = " "
 	if index == 0:
 		delim = "|"
 
-	values = line.split(',')
-	for value in values:
-		if value == "":
-			toPrint = toPrint + spacesPadding + value + (" " * longestValueInLine[index]) + spacesPadding + delim
+	vals = ln.split(',')
+	for i, val in enumerate(vals):
+		if(i == len(vals) - 1):
+			formattedLine = "{}{}{}".format(formattedLine, pad, val)
+		elif val == "":
+			offsetSpaces = " " * longestValueInLine
+			formattedLine = "{}{}{}{}{}{}".format(formattedLine, pad, val, offsetSpaces, pad, delim)
 		else:
-			toPrint = toPrint + spacesPadding + value + (" " * (longestValueInLine[index] - len(value)) + spacesPadding + delim
-	print toPrint
+			offsetValue = longestValueInLine - len(val)
+			offsetSpaces = " " * offsetValue
+			formattedLine = "{}{}{}{}{}{}".format(formattedLine, pad, val, offsetSpaces, pad, delim)
+	print formattedLine
