58a976735e1b4c605ab5786e1cfb06c12b003032
Diane O'Connell <diane.oconnell@gmail.com>
2015-05-14 21:17:24 -0400

    Create twitter.py

---
+++ twitter.py
@@ -0,0 +1,16 @@
+#twitter - access to twitter api - this requires tweepy be installed
+
+import tweepy
+
+consumer_key = ''
+
+consumer_secret = ''
+
+access_key = ''
+
+access_secret = ''
+
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_key, access_secret)
+
+api = tweepy.API(auth)
